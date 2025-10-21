c3321f2fc64a9aacf72e57408a21bba8cf4d4c90
llbjie <llbjie@outlook.com>
2025-10-21 19:55:36 +0800

    在helper_string.py中添加sdkFindFilePath函数

--- cuda_bindings/examples/common/helper_string.py
+++ cuda_bindings/examples/common/helper_string.py
@@ -2,7 +2,8 @@
 # SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
 
 import sys
-
+import os
+import logging
 
 def checkCmdLineFlag(stringRef):
     return any(stringRef == i and k < len(sys.argv) - 1 for i, k in enumerate(sys.argv))
@@ -13,3 +14,31 @@ def getCmdLineArgumentInt(stringRef):
         if stringRef == i and k < len(sys.argv) - 1:
             return sys.argv[k + 1]
     return 0
+
+
+def sdkFindFilePath(filename, executable_path):
+    """ 从executable_path的目录中查找文件的路径
+    
+    :filename: 要查找的文件名
+    :executable_path: 可执行文件的路径
+    
+    """
+
+    platform = sys.platform
+    if platform.startswith("win32") :
+        delimiter_pos = executable_path.rfind("\\")
+        executable_dir = executable_path[:delimiter_pos + 1] if delimiter_pos != -1 else str
+
+    else:
+        delimiter_pos = executable_path.rfind("/")
+        executable_dir = executable_path[:delimiter_pos + 1] if delimiter_pos != -1 else str
+    
+    path = executable_dir + filename
+    try:
+        print(os.path.abspath(path))
+        with open(path, 'r') as f:
+            return path
+    except FileNotFoundError:
+        pass
+
+    return None
